052fb9130d80cad4d5e659e05908ca956a04bbc6
Jason Jung <akogenweb@gmail.com>
2022-11-04 10:14:41 -0700

    GraphQL Djanog Query

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .schema import schema
+from strawberry.django.views import GraphQLView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +28,5 @@ urlpatterns = [
     path("api/v1/rooms/", include("rooms.urls")),
     path("api/v1/users/", include("users.urls")),
     path("api/v1/wishlists/", include("wishlists.urls")),
+    path("graphql/", GraphQLView.as_view(schema=schema)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
